8b4d9bedcdad7f5a6ac778d6b363d9fccf049fea
J-Bergfeld <155997219+J-Bergfeld@users.noreply.github.com>
2026-02-09 13:22:52 -0800

    kfugk.u

--- p2/tim2_waveout.c
+++ p2/tim2_waveout.c
@@ -4,23 +4,38 @@
 #include "wavegen.h"
 #include "dac.h"
 
-static void tim2_program_rate(uint32_t wave_freq_hz)
+#include "tim2_waveout.h"
+#include "stm32l476xx.h"
+#include "wave_tables.h"
+
+void tim2_program_rate(uint32_t wave_freq_hz)
 {
     uint32_t fs = wave_freq_hz * WAVE_TABLE_LEN;
 
-    uint32_t tim_clk = SystemCoreClock;
+    uint32_t tim_clk = SystemCoreClock;   // OK if SystemCoreClock is correct in Cube
     uint32_t psc = 0;
-    uint32_t arr = (tim_clk / fs) - 1U;
+    uint64_t arr = (tim_clk / fs) - 1U;
 
     while (arr > 0xFFFFU) {
         psc++;
         arr = (tim_clk / (fs * (psc + 1U))) - 1U;
     }
-
+    TIM2->DIER &= ~TIM_DIER_UIE;
+    TIM2->CR1 &= ~TIM_CR1_CEN;    // NEW: stop timer before changing PSC/ARR
     TIM2->PSC = psc;
     TIM2->ARR = arr;
-    TIM2->EGR = TIM_EGR_UG;
+    TIM2->EGR = TIM_EGR_UG;       // NEW: force update event to load PSC/ARR
+    TIM2->SR  &= ~TIM_SR_UIF;     // NEW: clear pending update flag
+    TIM2->CR1 |= TIM_CR1_CEN;     // NEW: restart timer
+    TIM2->DIER |= TIM_DIER_UIE;
+    return;
 }
+
+void tim2_waveout_set_wave_freq(uint32_t wave_freq_hz)
+{
+    tim2_program_rate(wave_freq_hz);
+}
+
 
 void tim2_waveout_init(uint32_t wave_freq_hz)
 {
@@ -32,14 +47,12 @@ void tim2_waveout_init(uint32_t wave_freq_hz)
 
     NVIC_SetPriority(TIM2_IRQn, 1);
     NVIC_EnableIRQ(TIM2_IRQn);
-
+    TIM2->SR  &= ~TIM_SR_UIF;     // NEW: clear pending update flag
+    TIM2->CR1 |= TIM_CR1_ARPE;
     TIM2->CR1 |= TIM_CR1_CEN;
+    __enable_irq();
 }
 
-void tim2_waveout_set_wave_freq(uint32_t wave_freq_hz)
-{
-    tim2_program_rate(wave_freq_hz);
-}
 
 void TIM2_IRQHandler(void)
 {
